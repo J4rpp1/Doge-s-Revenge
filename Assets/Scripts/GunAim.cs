1c14507f88f4a07cf83badf96b700c12b6387505
Spazi <talishmarprkle+g@gmail.com>
2021-12-07 00:42:57 +0200

    Updated animations, shibe model, animation code

--- Assets/Scripts/GunAim.cs
+++ Assets/Scripts/GunAim.cs
@@ -15,6 +15,13 @@ public class GunAim : MonoBehaviour
 	LayerMask rayMask;
 	[SerializeField]
 	RectTransform crosshair;
+	[SerializeField]
+	bool forceGunIntoHand;
+	public bool forceGunAim; //This variable would be controlled by animation keyframes or events
+	[SerializeField]
+	Transform handBone;
+	[SerializeField]
+	GameObject gunMesh;
 	Vector3 aimDesiredPosition;
 	Vector2 crosshairDesiredPosition;
 	void FixedUpdate()
@@ -23,20 +30,33 @@ public class GunAim : MonoBehaviour
 		// Does the ray intersect any objects excluding the player layer
 		if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, maxAimRange, rayMask))
 		{
-			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
+			//Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
 			aimDesiredPosition = hit.point;
 		}
 		else
 		{
-			Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxAimRange, Color.yellow);
+			//Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * maxAimRange, Color.yellow);
 			aimDesiredPosition = transform.position + transform.TransformDirection(Vector3.forward) * maxAimRange;
 		}
 	}
 	void Update()
 	{
-		//aimPoint.position = aimDesiredPosition;
 		aimPoint.position = Vector3.Lerp(aimPoint.position, aimDesiredPosition, 50f * Time.deltaTime); //wrong way to use lerp and probably wrong way to use Time.deltaTime but it should work
-		//crosshairDesiredPosition = new Vector2(aimPoint.position.x, aimPoint.position.y);
-		crosshair.position = cam.WorldToScreenPoint(aimPoint.position);
+		crosshair.position = cam.WorldToScreenPoint(aimPoint.position); //UI crosshair placed on physical aim point position
+
+		if(forceGunIntoHand)
+		PutGunInHand();
+	}
+	void PutGunInHand() //This is done to prevent inherited scale from animation. Easier than resetting global scale.
+	{
+		gunMesh.transform.position = handBone.position;
+		if(forceGunAim)
+		{
+			gunMesh.transform.LookAt(aimPoint.position, transform.up); //Point gun towards crosshair
+		}
+		else
+		{
+			gunMesh.transform.rotation = handBone.rotation; //Plainly attach gun to hand. This works if the LookAt or Animation Riggin solutions fail
+		}
 	}
 }
--- Assets/Scripts/PlayerAnimation.cs
+++ Assets/Scripts/PlayerAnimation.cs
@@ -6,13 +6,7 @@ public class PlayerAnimation : MonoBehaviour
 {
 	Animator anim; 
 	[SerializeField]
-	GameObject gunMesh;
-	[SerializeField]
 	Transform cameraPivot;
-	[SerializeField]
-	bool forceGunIntoHand;
-	[SerializeField]
-	Transform handBone;
 	float cameraInclination;
 	float maxCameraAngle = 60f; //this should always correspond to max and min camera angles
 	
@@ -23,20 +17,9 @@ public class PlayerAnimation : MonoBehaviour
 	}
 	void Update()
 	{
-		if(forceGunIntoHand)
-		PutGunInHand();
-
  		cameraInclination = cameraPivot.eulerAngles.x;
 		if(cameraPivot.eulerAngles.x > maxCameraAngle + 1f) // +1f for safety margin
 			cameraInclination = cameraPivot.eulerAngles.x - 360f; // camera Transform pitch jumps over to 360 below 0 so we need to subtract to get it into 60 to -60 range
 		anim.SetFloat("AimInclination", cameraInclination / maxCameraAngle); // remap from "60 to -60" to "1 to -1"
-		
-
-	}
-
-	void PutGunInHand() //This is done to prevent inherited scale from animation. Easier than resetting global scale.
-	{
-		gunMesh.transform.position = handBone.position;
-		gunMesh.transform.rotation = handBone.rotation; //Rotation would be handled by Animation Rigging m_GunAim if it worked
 	}
 }
