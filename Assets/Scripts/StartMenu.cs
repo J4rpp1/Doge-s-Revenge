0b8b5532a945eeffd6db7b505f9f291afb9416f7
tarmodev <tarmo.pelifarmi@gmail.com>
2021-12-07 10:27:42 +0200

    Move code from StartMenu.cs to VolumeUserInterface.cs, plus other changes

--- Assets/Scripts/StartMenu.cs
+++ Assets/Scripts/StartMenu.cs
@@ -2,28 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using TMPro;
 
 public class StartMenu : MonoBehaviour {
-    
-    public Slider volumeSlider;
-    public TMP_InputField volumeInputField;
-
-
-    public StartMenu() {
-        currentText = (currentGameVolume * 100f).ToString("f0");
-        safeText = currentText;
-
-    }
-
-    public float currentGameVolume { get; private set; } = 1 ;
-    [SerializeField]
-    private string currentText;
-    private string safeText;
-
-    
-
     public void OnPlayButtonPressed() {
         SceneManager.LoadScene("Game");
 
@@ -36,52 +16,7 @@ public class StartMenu : MonoBehaviour {
         Application.Quit();
 
     }
-    private void UpdateVolumeUserInterface() {
+    
 
-    }
-
-    public void ParseStuff() {
-
-    }
-    public void OnVolumeSliderValueChanged() { 
-        currentGameVolume = volumeSlider.value;
-        volumeInputField.text = volumeInputField.text = (currentGameVolume * 100f).ToString("f0");
-        safeText = (currentGameVolume * 100f).ToString("f0");
-
-        Debug.Log("The value of gameVolume is " + currentGameVolume.ToString());
-
-    }
-    public void OnPercentageInputFieldEditEnded() {
-        try {
-            currentGameVolume = float.Parse(volumeInputField.text) * (1f / 100f);
-
-        }
-        catch (System.FormatException) {
-            currentGameVolume = float.Parse(safeText) * (1f / 100f);
-
-        }
-
-        volumeInputField.text = (currentGameVolume * 100f).ToString("f0");
-        safeText = (currentGameVolume * 100f).ToString("f0");
-        volumeSlider.value = currentGameVolume;
-
-        Debug.Log("The value of gameVolume is " + currentGameVolume.ToString());
-    }
-
-    private void Start() {
-        volumeSlider.onValueChanged.AddListener(delegate {
-            OnVolumeSliderValueChanged();
-        });
-        volumeInputField.onEndEdit.AddListener(delegate {
-            OnPercentageInputFieldEditEnded();
-        });
-
-        volumeInputField.text = currentText;
-
-    }
-    private void Update() {
-        
-
-    }
 
 }
--- Assets/Scripts/VolumeUserInterface.cs
+++ Assets/Scripts/VolumeUserInterface.cs
@@ -1,11 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeUserInterface : MonoBehaviour
 {
-    //private float volume = 1.0f;
-    private void Start() {
-        
+    public Slider volumeSlider;
+    public TMP_InputField volumeInputField;
+
+    public float CurrentGameVolume { get; private set; } = 1;
+    [SerializeField]
+    private string currentText = "100";
+    private string safeText;
+
+
+
+
+
+    public void OnVolumeSliderValueChanged() {
+        CurrentGameVolume = volumeSlider.value;
+        volumeInputField.text = volumeInputField.text = (CurrentGameVolume * 100f).ToString("f0");
+        safeText = (CurrentGameVolume * 100f).ToString("f0");
+
+        Debug.Log("The value of gameVolume has been updated to " + CurrentGameVolume.ToString() + ".");
+
     }
+    public void OnPercentageInputFieldEditEnded() {
+        try {
+            CurrentGameVolume = float.Parse(volumeInputField.text) * (1f / 100f);
+
+        }
+        catch (System.Exception) {
+            CurrentGameVolume = float.Parse(safeText) * (1f / 100f);
+
+        }
+
+        volumeInputField.text = (CurrentGameVolume * 100f).ToString("f0");
+        safeText = (CurrentGameVolume * 100f).ToString("f0");
+        volumeSlider.value = CurrentGameVolume;
+
+        Debug.Log("The value of gameVolume has been updated to " + CurrentGameVolume.ToString() + ".");
+
+    }
+
+    private void Start() {
+        currentText = (CurrentGameVolume * 100f).ToString("f0");
+        safeText = currentText;
+
+        volumeSlider.onValueChanged.AddListener(delegate {
+            OnVolumeSliderValueChanged();
+        });
+        volumeInputField.onEndEdit.AddListener(delegate {
+            OnPercentageInputFieldEditEnded();
+        });
+
+        volumeInputField.text = currentText;
+
+    }
+
 }
